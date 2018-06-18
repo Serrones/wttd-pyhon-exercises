7d7531ed90482f0ba9c3a603139220dc752c5b5e
Serrones <fabioserrones@gmail.com>
2018-06-17 23:02:33 -0300

    testes e coding dojo

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -26,21 +26,3 @@ def robot(pos):
     if multiplo_5(pos) and multiplo_3(pos):
         say = 'FizzBuzz'
     return say
-
-
-if __name__ == '__main__':
-    assert robot(1) == '1'
-    assert robot(2) == '2'
-    assert robot(4) == '4'
-
-    assert robot(3) == 'Fizz'
-    assert robot(6) == 'Fizz'
-    assert robot(9) == 'Fizz'
-
-    assert robot(5) == 'Buzz'
-    assert robot(10) == 'Buzz'
-    assert robot(20) == 'Buzz'
-
-    assert robot(15) == 'FizzBuzz'
-    assert robot(30) == 'FizzBuzz'
-    assert robot(45) == 'FizzBuzz'
---
+++ test_fizzbuzz.py
@@ -0,0 +1,37 @@
+import unittest
+from fizzbuzz import robot
+
+
+class FizzBuzz(unittest.TestCase):
+    def test_say_1_when_1(self):
+        self.assertEqual(robot(1), '1')
+
+    def test_say_2_when_2(self):
+        self.assertEqual(robot(2), '2')
+
+    def test_say_4_when_4(self):
+        self.assertEqual(robot(4), '4')
+
+    def test_say_Fizz_when_3(self):
+        self.assertEqual(robot(3), 'Fizz')
+
+    def test_say_Fizz_when_6(self):
+        self.assertEqual(robot(6), 'Fizz')
+
+    def test_say_Fizz_when_9(self):
+        self.assertEqual(robot(9), 'Fizz')
+
+    def test_say_Buzz_when_5(self):
+        self.assertEqual(robot(5), 'Buzz')
+
+    def test_say_Buzz_when_10(self):
+        self.assertEqual(robot(10), 'Buzz')
+
+    def test_say_Buzz_when_20(self):
+        self.assertEqual(robot(20), 'Buzz')
+
+    def test_say_FizzBuzz_when_15(self):
+        self.assertEqual(robot(15), 'FizzBuzz')
+
+    def test_say_FizzBuzz_when_30(self):
+        self.assertEqual(robot(30), 'FizzBuzz')
